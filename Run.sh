cce5459fe7aa04209207c3bce42740e0ef24d864
CaDoCoda <dyhn55c6r4@privaterelay.appleid.com>
2026-03-08 23:14:57 -0600

    Clean up Run.sh by removing redundant lines
    Removed unnecessary commands for script execution.

--- Run.sh
+++ Run.sh
@@ -1,10 +1,4 @@
 echo' #!/bin/bash
-
-# Make the Swift script executable
 chmod +x run-apple.swift
-
-# Make this script executable (harmless if already executable)
 chmod +x run.sh
-
-# Run the Swift script
 ./run-apple.swift
